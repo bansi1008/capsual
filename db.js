18d38f7b37e3d1d1e36ad1025816c5e945b14369
bansi <bansidobariya2510@gmail.com>
2025-06-06 15:32:34 +0100

    db changed

--- db.js
+++ db.js
@@ -5,19 +5,16 @@ const { Pool } = require("pg");
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
   ssl: {
-    rejectUnauthorized: true, // Enforce certificate verification
-    ca: process.env.DB_CA_CERT,
+    rejectUnauthorized: false,
   },
 });
 
-// Test the connection without exiting the process
 (async () => {
   try {
     const res = await pool.query("SELECT NOW()");
     console.log("DB connected at:", res.rows[0].now);
   } catch (err) {
     console.error("Connection error:", err);
-    // Don't exit the process, just log the error
   }
 })();
 
