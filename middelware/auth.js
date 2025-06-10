540a6ba2150adafada688669dcdbe9fe8e20c9ff
bansi <bansidobariya2510@gmail.com>
2025-06-10 19:45:13 +0100

    Removed sensitive data

--- middelware/auth.js
+++ middelware/auth.js
@@ -3,7 +3,7 @@ const jwt = require("jsonwebtoken");
 const auth = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
     return res.status(401).json({ error: "Unauthorized access" });
   }
   const token = authHeader.split(" ")[1];
--- routing/capsualrouting.js
+++ routing/capsualrouting.js
@@ -1,12 +1,16 @@
 const express = require("express");
+
 const router = express.Router();
 const {
   genratecapsual,
+  getCapsules,
   sendPendingCapsules,
+  generateMessage,
 } = require("../controller/idcreation.js");
 const auth = require("../middelware/auth.js");
 
 router.post("/capsual", auth, genratecapsual);
-router.get("/send-pending-capsules", auth, sendPendingCapsules);
+router.get("/sendpendingcapsules", sendPendingCapsules);
+router.post("/ai", generateMessage);
 
 module.exports = router;
